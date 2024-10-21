24452b5f76fcc3401608f679ae90a14ab3560a0b
Subash <spidybeatz25@gmail.com>
2024-10-21 10:29:19 +0530

    Initial commit

---
+++ revshop-main/src/main/java/com/revshop/main/revshop_main/RevshopMainApplication.java
@@ -0,0 +1,13 @@
+package com.revshop.main.revshop_main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevshopMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevshopMainApplication.class, args);
+	}
+
+}
---
+++ revshop-main/src/test/java/com/revshop/main/revshop_main/RevshopMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revshop.main.revshop_main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevshopMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
